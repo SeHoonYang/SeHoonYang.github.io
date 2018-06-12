2b4f7602e2a8f03b7c8c1cd075db915a7bfede64
SeHoon Yang <apaphpmys@kaist.ac.kr>
2018-06-12 20:27:00 +0900

    different path on mobile

--- svg.js
+++ svg.js
@@ -1,3 +1,8 @@
+// mobile detection
+var mobile = false;
+if (window.innerWidth <= 800)
+	mobile = true;
+
 function NormalColorShader(lightList, worldPosition, normal, baseColor, finalColor){
 	finalColor[0] = Math.floor((Math.floor(normal[0] * 128) + 128)*255/256);
 	finalColor[1] = Math.floor((Math.floor(normal[1] * 128) + 128)*255/256);
@@ -26,26 +31,44 @@ var fovy = 70;
 var mainCamera = context.createCamera(camPosition, camRotation, near, far, viewPortWidth / viewPortHeight, fovy);
 
 var pLight1 = context.createPointLight([0,-5,-2], 0xFF0000, 0.5);
+
 // Mouse manipulation
 var ViewPort = document.getElementById("viewportContainer");
 var x = viewPortWidth / 2;
 var y = viewPortHeight / 2;
-onmousemove = function(e){
-	var vp = ViewPort.getBoundingClientRect();
-	x = Math.max(0, Math.min(viewPortWidth, e.clientX - vp.left));
-	y = Math.max(0, Math.min(viewPortHeight, e.clientY - vp.top));
-}
 
-setInterval(loop, 25);
+if (!mobile)
+{
+	onmousemove = function(e){
+		var vp = ViewPort.getBoundingClientRect();
+		x = Math.max(0, Math.min(viewPortWidth, e.clientX - vp.left));
+		y = Math.max(0, Math.min(viewPortHeight, e.clientY - vp.top));
+	}
+}
+var loopInterval = 25;
+if (mobile)
+	loopInterval = 50;
+
+setInterval(loop, loopInterval);
 
 function loop() {
 	// Apply  rotation
 	var rot_q = quat.create();
-	var rot_m = mat4.create();		
-	quat.fromEuler(rot_q, - (y / viewPortHeight - 0.5) * 180, (x / viewPortWidth - 0.5) * 180, 0);
-	mat4.fromQuat(rot_m, rot_q);
-	mat4.multiply(rot_m, rot_m, bunnyRotMat);
-	bunny.rotateTo(rot_m);
+	var rot_m = mat4.create();
 
+	if(!mobile)
+	{
+		quat.fromEuler(rot_q, - (y / viewPortHeight - 0.5) * 180, (x / viewPortWidth - 0.5) * 180, 0);
+		mat4.fromQuat(rot_m, rot_q);
+		mat4.multiply(rot_m, rot_m, bunnyRotMat);
+		bunny.rotateTo(rot_m);
+	}
+	else
+	{
+		quat.fromEuler(rot_q, 2,5,3);
+		mat4.fromQuat(rot_m, rot_q);
+		mat4.multiply(rot_m, rot_m, bunny.getRotMat());
+		bunny.rotateTo(rot_m);
+	}
 	context.draw3d();
 }
